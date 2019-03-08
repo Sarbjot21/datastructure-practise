3c70219b4b7674dce841ef1cbf668d07ef9388c5
SSingh <SSingh@A2AIGML17720.mindtree_odc01.com>
2019-03-08 12:46:12 +0530

    Initial Commit

---
+++ src/algorithm/search/LinearSearch.java
@@ -0,0 +1,42 @@
+/*Problem: Given an array arr[] of n elements,
+write a function to search a given element x in arr[].
+Examples :
+Input : arr[] = {10, 20, 80, 30, 60, 50, 110, 100, 130, 170}
+        x = 110;
+Output : 6
+Element x is present at index 6
+Input : arr[] = {10, 20, 80, 30, 60, 50,110, 100, 130, 170}
+        x = 175;
+Output : -1
+Element x is not present in arr[].*/
+
+package algorithm.search;
+
+
+public class LinearSearch {
+
+	public static void main(String args[]) 
+	{ 
+	    int arr[] = { 2, 3, 4, 10, 40 };  
+	    int x = 10; 
+	      
+	    int result = search(arr, x); 
+	    if(result == -1) 
+	        System.out.print("Element is not present in array"); 
+	    else
+	        System.out.print("Element is present at index " + result); 
+	}
+
+	private static int search(int[] arr, int x)
+	    {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<arr.length) {
+		    if(arr[i]==x)
+			return (i+1);
+		    i++;
+		}
+		return -1;
+	    } 
+
+}
