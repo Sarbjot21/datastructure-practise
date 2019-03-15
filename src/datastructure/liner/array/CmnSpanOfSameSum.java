893338db7efa0af51dc56b2e4d9f83051628ccfa
SSingh <SSingh@A2AIGML17720.mindtree_odc01.com>
2019-03-16 00:24:27 +0530

    Added Array

---
+++ src/datastructure/liner/array/CmnSpanOfSameSum.java
@@ -0,0 +1,40 @@
+package datastructure.liner.array;
+/*
+ * 
+ * Qs : Longest Span with same Sum in two Binary arrays | GeeksforGeeks
+ * 
+ * Explanation at https://www.youtube.com/watch?v=xtfj4-r_Ahs&list=PLqM7alHXFySEQDk2MDfbwEdjd2svVJH9p
+ *  
+ *  Status : Not Completed
+*/
+class CmnSpanOfSameSum 
+{ 
+	/*static int arr1[] = new int[]{0, 1, 0, 1, 1, 1, 1}; 
+	static int arr2[] = new int[]{1, 1, 1, 1, 1, 0, 1}; 
+	*/
+    	static int arr1[] = {1,	0, 1,0};
+    	static int arr2[] = {1, 1, 1,1};
+	// Returns length of the longest common sum in arr1[] 
+	// and arr2[]. Both are of same size n. 
+	static int longestCommonSum(int n) 
+	{ 
+		//int maxLen=0;
+		int i=0;
+		int sum=0;
+		while(i<n) {
+		    sum+=(arr1[i]-arr2[i]);
+		    i++;
+		}
+		
+		return (n+sum);
+		
+		
+	} 
+	
+	// Driver method to test the above function 
+	public static void main(String[] args) 
+	{ 
+		System.out.print("Length of the longest common span with same sum is "); 
+		System.out.println(longestCommonSum(arr1.length)); 
+	} 
+} 
---
+++ src/datastructure/liner/array/ReverseArray.java
@@ -0,0 +1,49 @@
+package datastructure.liner.array;
+
+import java.io.*;
+import java.util.*;
+
+public class ReverseArray
+{
+
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a) {
+        int it=a.length/2;
+        int size=a.length-1;
+        int i=0;
+        int temp=0;
+        while(i<it){
+            temp=a[i];
+            a[i++]=a[size];
+            a[size--]=temp;
+        }
+        return a;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("./output.txt"));
+
+        int arrCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] res = reverseArray(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i]+" ");
+        }
+
+        scanner.close();
+    }
+}
