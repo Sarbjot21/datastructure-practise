c4ca2f5ba7a2389075e1af83c3167b8002eba983
Sarbjot Singh <srbjtsingh94@gmail.com>
2019-04-21 19:13:11 +0530

    Updated funcname
    Signed-off-by: sarbsing <sarbjot.singh@aig.com>

--- src/algorithm/search/binary/BinarySearch.java
+++ src/algorithm/search/binary/BinarySearch.java
@@ -17,28 +17,36 @@ public class BinarySearch
 	        int arr[] = { 1, 3, 5,10, 10, 40,50,80,90 }; 
 	        int n = arr.length; 
 	        int x = 90; 
-	        int result = ob.binarySearch(arr, 0, n - 1, x); 
+	        int result = ob.binarySearchRecursion(arr, 0, n - 1, x); 
 	        if (result == -1) 
 	            System.out.println("Element not present"); 
 	        else
 	            System.out.println("Element found at index " + result); 
 	    }
 
-	private int binarySearch(int[] arr, int start, int end, int value)
+	private int binarySearchRecursion(int[] arr, int start, int end, int value)
 	    {
-		System.out.println("Executing it start:"+start+" end: "+end);
-		if(end>0) {
-		    
-        		int mid=(start+end)/2;
-        		if(arr[mid]==value)
-        		    return mid;
-        		else if(arr[mid]>value)
-        		    return binarySearch(arr, start, mid-1, value);
-        		else
-        		    return binarySearch(arr, mid+1, end, value);
-		}
-		return -1;
-	    } 
+			System.out.println("Executing it start:"+start+" end: "+end);
+			if(end>0) {
+			    
+	        		int mid=(start+end)/2;	//1
+	        		if(arr[mid]==value)
+	        		    return mid;
+	        		else if(arr[mid]>value)
+	        		    return binarySearchRecursion(arr, start, mid-1, value);
+	        		else
+	        		    return binarySearchRecursion(arr, mid+1, end, value);
+			}
+			return -1;
+	    }
+	}
 	
-	
-    }
+    
+
+/*Note
+ * 1. OR it could be start+(end-start)/2 to resolve the integer limit issue. because start+end could overflows the limit of integer, even though start and end are within limits individually.
+ * 
+ * 
+*/
+ 
+
